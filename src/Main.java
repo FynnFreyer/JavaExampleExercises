23a8922379f5c6498c739139fa885da16ab7667d
Fynn Freyer <fynn.freyer@googlemail.com>
2024-12-23 16:00:39 +0100

    feat: add Main class as entry point

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO:
+        //  use this to play around with the examples
+        //  or create main methods in the respective files
+        System.out.println("Hello World");
+    }
+}
